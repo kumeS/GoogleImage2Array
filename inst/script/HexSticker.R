54bd406901d38ddeaa8852b1179b08e058cfa044
kumeS <satoshi.kume@riken.jp>
2021-11-12 16:19:32 +0900

    v-0.99.6

---
+++ inst/script/HexSticker.R
@@ -0,0 +1,11 @@
+library(hexSticker)
+
+imgurl <- system.file("/inst/images/spiral.png",
+                      package="GoogleImage2Array")
+sticker(imgurl, package="GoogleImage2Array", p_size=22, s_x=1, s_y=.78, s_width=.58,
+        filename="./GoogleImage2Array/inst/images/hexSticker_GoogleImage2Array.png",
+        dpi = 500, h_fill = "#1d394a")
+
+
+
+
